6b2c4f76e7592a228f558d0e374f43eebf21c15f
Marc Williamson <marxwillia@gmail.com>
2017-09-05 19:53:29 -0400

    Create riemann.py

---
+++ riemann.py
@@ -0,0 +1,15 @@
+# This is a python file! The '#' character indicates that the following line is a comment.
+
+# The following is an example for how to define a function in Python
+# def tells the compiler that hello_world is the name of a function
+# this implementation of hello_world takes a string as an argument,
+# which has default value of the empty string. If the user calls 
+# hello_world() without an argument, then the compiler uses ''
+# as the default value of the argument.
+def hello_world(name=''):
+    print "hello world!"
+    print name
+    return
+    
+    
+#Implement the Riemann Sum approximation for integrals.
